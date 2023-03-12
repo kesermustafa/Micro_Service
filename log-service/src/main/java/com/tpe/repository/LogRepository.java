84aa4edd11c650e95e4b6be7f43dd0128cc91e86
Mustafa Keser <keserzade@gmail.com>
2023-03-12 18:48:30 +0300

    micro

---
+++ log-service/src/main/java/com/tpe/repository/LogRepository.java
@@ -0,0 +1,11 @@
+package com.tpe.repository;
+
+import com.tpe.domain.AppLog;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LogRepository extends MongoRepository<AppLog, String> {
+                            // !!! JpaRepository yerine MongoRepository yazdik...
+
+}
